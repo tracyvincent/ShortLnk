62111a59a8f2b31b1036c881fa33dfea1d3e4320
tracyvincent <tracykvincent@gmail.com>
2017-04-23 23:40:44 -0500

    Sets up basic link saving and publication/subscription abilities

---
+++ imports/api/links.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+export const Links = new Mongo.Collection('links');
+
+
+if (Meteor.isServer) {
+  Meteor.publish('links', () => {
+    return Links.find({url: 'link 2'});
+  })
+}
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 
+import { Links } from './../api/links';
+import LinksList from './LinksList';
 
 export default class Link extends React.Component {
   onLogout() {
     Accounts.logout();
+  }
+  onSubmit(event) {
+    const url = this.refs.url.value.trim();
+
+    event.preventDefault();
+
+    if (url) {
+      Links.insert({ url });
+      this.refs.url.value = '';
+    }
   }
   render() {
     return (
       <div>
         <h1>Your Links</h1>
         <button onClick={this.onLogout.bind(this)}>Log out</button>
+      <LinksList/>
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="url" placeholder="URL" />
+          <button>Add Link</button>
+        </form>
       </div>
     )
   }
