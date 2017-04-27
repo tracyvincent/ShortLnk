4ca150664f2c7f75df7c5b07c6ad4f00f01c22f5
tracyvincent <tracykvincent@gmail.com>
2017-04-27 00:02:14 -0500

    extends uuser expierence of copy button

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      justCopied: false
+    };
+  }
   componentDidMount() {
     this.clipboard = new Clipboard(this.refs.copy);
 
     this.clipboard.on('success', () => {
-      alert('It worked!');
+      this.setState({justCopied: true});
+      setTimeout(() => this.setState({justCopied: false}), 1000);
     }).on('error', () => {
       alert('Unable to copy. Please manually copy the link.');
     })
@@ -19,7 +26,9 @@ export default class LinksListItem extends React.Component{
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+          {this.state.justCopied ? 'Copied' : 'Copy'}
+        </button>
       </div>
     );
   }
