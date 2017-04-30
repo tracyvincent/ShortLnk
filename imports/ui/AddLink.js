41db9918a6c5af9350120eecaaf3b659590c0e5c
tracyvincent <tracykvincent@gmail.com>
2017-04-29 23:12:24 -0500

    makes hidden links checkbox a controlled input

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 
 export default class AddLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    };
+  }
 
   onSubmit(event) {
-    const url = this.refs.url.value.trim();
+    const url = this.state.url;
 
     event.preventDefault();
 
     if (url) {
-      Meteor.call('links.insert', url);
-      this.refs.url.value = '';
+      Meteor.call('links.insert', url, (err, res) => {
+        if (!err) {
+          this.setState({url: ''});
+        }
+      });
     }
+  }
+  onChange(event){
+    this.setState({
+      url: event.target.value
+    });
   }
 
   render(){
@@ -19,7 +33,11 @@ export default class AddLink extends React.Component {
       <div>
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL" />
+          <input
+            type="text"
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.onChange.bind(this)}/>
           <button>Add Link</button>
         </form>
       </div>
--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import {Session} from 'meteor/session';
+import {Tracker} from 'meteor/tracker';
 
-export default () => {
-  return (
-    <div>
-      <label>
-        <input type="checkbox" onChange={(event) => {
-          Session.set('showVisible', !event.target.checked);
-        }}/>
-        show hidden links
-      </label>
-    </div>
-  );
+export default class LinksListFilters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVisible: true
+    };
+  }
+  componentDidMount() {
+    this.checkedTracker = Tracker.autorun(() => {
+      this.setState({
+        showVisible: Session.get('showVisible')
+      })
+    });
+  }
+  componentWillUnmount() {
+    this.checkedTracker.stop();
+  }
+  render() {
+    return (
+      <div>
+        <label>
+          <input type="checkbox" checked={!this.state.showVisible} onChange={(event) => {
+            Session.set('showVisible', !event.target.checked);
+          }}/>
+          show hidden links
+        </label>
+      </div>
+    );
+  }
 };
