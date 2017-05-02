447db1f1e9a671585c12df71d06de4278c3dc37a
tracyvincent <tracykvincent@gmail.com>
2017-05-02 00:14:57 -0500

    styles sign up and login pages

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default () => {
   return (
-    <p>NotFound Component</p>
+    <div className="boxed-view">
+      <div className="boxed-view__box">
+        <h1>Page Not Found</h1>
+        <p>Hmmm, we're unable to find that page</p>
+        <Link to="/">Head Home</Link>
+      </div>
+    </div>
   )
 }
