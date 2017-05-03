2a95e588049bf0ed5982c0a8e7b7a6d8250b31e9
tracyvincent <tracykvincent@gmail.com>
2017-05-03 14:53:54 -0500

    impliments flip move

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
+import FlipMove from 'react-flip-move';
 
 import { Links } from './../api/links';
 import LinksListItem from './LinksListItem';
@@ -45,9 +46,9 @@ export default class LinksList extends React.Component {
   render() {
     return (
       <div>
-        <div>
+        <FlipMove maintainContainerHeight={true}>
           {this.renderLinksListItems()}
-        </div>
+        </FlipMove>
       </div>
     );
   }
